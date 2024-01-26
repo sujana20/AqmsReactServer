53bc50af2c1d0ee0bcbe1ef21f348e4aa3989736
shanmu <shanmukha.vattem@ispatialtec.com>
2024-01-25 18:45:47 -0600

    moved the url to env file

--- src/Pages/AIReport.js
+++ src/Pages/AIReport.js
@@ -282,7 +282,7 @@ function AIReport() {
   const getJsondata = async function (question) {
     let params = new URLSearchParams({ userMessage: question });
     try {
-      const response = await fetch("http://127.0.0.1:5000/conversation1?" + params, {
+      const response = await fetch(process.env.REACT_APP_CHATGPT_API + params, {
         method: 'GET',
       });
       const data = await response.json();
